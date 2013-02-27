10a4a19118c304301afc6d48ea85d0d6b5b765ea
Mark Rop <kemrop@gmail.com>
2013-02-26 22:10:04 -0600

    Latest and greatest invent

--- index.php
+++ index.php
@@ -1,74 +1,161 @@
-<!DOCTYPE html>
-<html>
-  <head>
-  <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
-  <title>Invent</title>
-  <script type="text/javascript" src="/scripts/jquery-1.8.3.min.js"></script>
-  <link rel="stylesheet" type="text/css" href="styles/styles.css">
-  <script type="text/javascript">
-    $(document).ready(function() {
-  	  //put all your jQuery goodness in here.
-   	 $('#about').click(function(){
-   	   	 if($('.work').is(':visible')) {
-   	   	    $('.work').toggle(1000);
-   	   	    $('.about').toggle(1000);
-   	   	 } 
-   		    $('.about').toggle(1000);
-   	 });  
-	   $('#work').click(function(){
-		  	 if($('.about').is(':visible')) {
-     	    $('.about ').toggle(1000);
-     	    $('.work').toggle(1000);
-		   	  } 
-       $('.work').toggle(1000);
-   	 });
-  	 });
- 	 
-  </script>
-  </head>
-  <body class='invent-body'>
-  <div id="left-panel">
-    <img id="logo" alt="Invent Design logo" src="images/InventLogo.bmp">
-  	<ul>
-  	  <li id="about">About</li>
-  	  <li id="work">Work</li>
-  	
-  	</ul>
-	 </div>
-	 <div id="middle-panel">
-  	  <div class="about slide-out">
-  	    <div class="content-fixed">
-    	   <h1> About us</h1>
-      	    Lorem Ipsum is simply dummy text of the printing and
-      	     typesetting industry. Lorem Ipsum has been the industry's 
-      	     standard dummy text ever since the 1500s, when an unknown printer 
-      	     took a galley of type and scrambled it to make a type specimen book. 
-      	     It has survived not only five centuries, but also the leap into electronic 
-      	     typesetting, remaining essentially unchanged. It was popularised in the 1960s
-      	      with the release of Letraset sheets containing Lorem Ipsum passages, 
-      	    and more recently with desktop publishing software like Aldus PageMaker 
-      	    including versions of Lorem Ipsum.
-  	    
-  	    </div>
-      </div>
-  	  <div class="work slide-out">
-  	  <div class="content-fixed">
-  	  <h1> How we work</h1>
-      	    Lorem Ipsum is simply dummy text of the printing and
-      	     typesetting industry. Lorem Ipsum has been the industry's 
-      	     standard dummy text ever since the 1500s, when an unknown printer 
-      	     took a galley of type and scrambled it to make a type specimen book. 
-      	     It has survived not only five centuries, but also the leap into electronic 
-      	     typesetting, remaining essentially unchanged. It was popularised in the 1960s
-      	      with the release of Letraset sheets containing Lorem Ipsum passages, 
-      	    and more recently with desktop publishing software like Aldus PageMaker 
-      	    including versions of Lorem Ipsum.
-    	 </div>
-  	  
-  	  </div>
-	 </div>
-	 <div id="right-panel">
-	 Some text goes here!!
-	 </div>
-  </body>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" lang="en-US">
+
+	<head profile="http://gmpg.org/xfn/11">
+
+	<title>Invent Creative Designs</title>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	
+	<script type="text/javascript" src="/scripts/jquery-1.9.1.min.js"></script>
+	<script src="http://dev.jquery.com/view/tags/ui/latest/ui/effects.core.js"></script>
+	<script src="http://dev.jquery.com/view/tags/ui/latest/ui/effects.slide.js"></script>
+	<script type="text/javascript" src="supersized.2.0.js"></script>
+	<script type="text/javascript">  
+		$(function(){
+			$.fn.supersized.options = {  
+				startwidth: 640,  
+				startheight: 480,
+				vertical_center: 1,
+				slideshow: 1,
+				navigation: 1,
+				transition: 1, //0-None, 1-Fade, 2-slide top, 3-slide right, 4-slide bottom, 5-slide left
+				pause_hover: 0,
+				slide_counter: 1,
+				slide_captions: 1,
+				slide_interval: 3000  
+			};
+	        $('#supersize').supersized(); 
+	    });
+	</script>
+	
+	<style type="text/css">
+		/**{
+			margin:0;
+			padding:0;
+		}*/
+		a{
+			color:#8FC2FF;
+			text-decoration: none;
+			outline: none;
+		}
+		a:hover{
+			text-decoration: underline;
+		}
+		img{
+			border:none;
+		}
+		body {
+			overflow:hidden;/*Needed to eliminate scrollbars*/
+			background:#FFFFFF;
+		}
+		#content{
+			margin-left:200px;
+			height:100px;
+			width:80%;
+			bottom:5%;
+			z-index: 3;
+			position:absolute;
+		}
+		#contentframe{
+			overflow: hidden;
+			/*border-top:solid 1px #4F4F4F;
+			border-bottom:1px solid #000;*/
+			height: 100%;
+			text-align:left;
+			z-index: 3;
+		}
+		#slidecounter{
+			float:left;
+			color:#4F4F4F;
+			font:50px "Helvetica Neue", Arial, sans-serif;
+			font-weight:bold;
+			margin:18px 20px;	
+		}
+		#slidecaption{
+			overflow: hidden;
+			float:left;
+			color:#FFF;
+			font:26px "Helvetica Neue", Arial, sans-serif;
+			font-weight:bold;
+			margin:33px 0;
+		}
+		/*Supersized Stamp*/
+		.stamp{
+			float: right;
+			margin: 25px 20px 0 0;
+		}
+		/*Supersize Plugin Styles*/
+		#navigation{
+			background: url('images/navbg.gif') no-repeat;
+			float: right;
+			margin:22px 20px 0 0;
+		}
+		#loading {
+			position: absolute;
+			top: 49.5%; 
+			left: 49.5%;
+			z-index: 3;
+			width: 24px; 
+			height: 24px;
+			text-indent: -999em;
+			background-image: url(images/progress.gif);
+		}
+		#supersize{
+			position:fixed;
+			margin: 200px 50px 100px 200px;
+		}
+		#supersize img, #supersize a{
+			height:55%;
+			width:80%;
+			position:absolute;
+			z-index: 0; 
+		}
+		#supersize .prevslide, #supersize .prevslide img{
+			z-index: 1;
+		}
+		#supersize .activeslide, #supersize .activeslide img{
+			z-index: 2;
+		}
+		
+		
+	</style>
+</head>
+
+<body>
+
+<!--Loading display while images load-->
+<div id="loading">&nbsp;</div>
+
+<!--Slides-->
+<div id="supersize">
+	<a href="#"><img src="images/bird.jpg" title="Bird On A Branch"/></a>
+	<a href="#"><img src="images/paradise.jpg" title="Paradise Lost"/></a>
+	<a href="#"><img src="images/snake.jpg" title="Morelia Viridis"/></a>
+</div>
+
+<!--Content area that hovers on top-->
+<div id="content">
+	<div id="contentframe">
+	
+		<div id="slidecounter"><!--Slide counter-->
+			<span class="slidenumber"></span>/<span class="totalslides"></span>
+		</div>
+		<div id="slidecaption"><!--Slide captions displayed here--></div>
+		<!--Logo-->
+	<!-- <a href="#" class="stamp"><img src="images/supersizedblackmini.gif"/></a> -->
+			<a href="#" class="stamp"><img src="images/Invent.png"/></a>
+		<!--Navigation-->
+		<div id="navigation">
+			<a href="#" id="prevslide">
+			<img src="images/back_dull.gif"/></a>
+			<a href="#" id="pauseplay">
+			<img src="images/pause_dull.gif"/></a>
+			<a href="#" id="nextslide">
+			<img src="images/forward_dull.gif"/></a>
+		</div>
+		
+	</div>
+</div>
+
+</body>
 </html>
